16483d7349558e15b2ad62471068fd03d4d7d32e
Szymofcion <Szymon.dziewajs@gmail.com>
2023-01-03 22:03:14 +0100

    added section MealItemForm

---
+++ src/components/Melas/MealsItem/MealItemForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './MealItemForm.module.css'
+
+const MealItem = (props) => {
+
+    return (
+        <form action="">
+            <input type="text" />
+            <button>Add</button>
+        </form>
+    )
+}
+
+export default MealItem
