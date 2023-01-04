064147b99ccd6363e7cd07742eee3ebfa6d91638
Szymofcion <Szymon.dziewajs@gmail.com>
2023-01-04 21:22:23 +0100

    try aplicated cart

--- src/components/Melas/MealsItem/MealItemForm.js
+++ src/components/Melas/MealsItem/MealItemForm.js
@@ -1,16 +1,22 @@
-import React from "react";
+import { useRef } from "react";
 import classes from './MealItemForm.module.css'
 import Input from "../../UI/Input";
+
 const MealItem = (props) => {
+    const amountInputRef = useRef();
+
+    const submitHandler = e => {
+        e.preventDefault();
+    }
 
     return (
-        <form className={classes.form}>
-            <Input label="Abount" input={{
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input label="Abount" ref={amountInputRef} input={{
                 id: 'amount',
                 type: 'number',
                 min: '1',
                 max: '5',
-                step:'1',
+                step: '1',
                 defaultValue: '1',
             }} />
             <button>+ Add</button>
