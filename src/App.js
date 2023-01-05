1dfc87d3593f2740d9888b48a66e9df55c5b08eb
Szymofcion <Szymon.dziewajs@gmail.com>
2023-01-05 12:10:42 +0100

    logic in Cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Melas/Meals";
-import Cart from './components/Cart/Cart'
+import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [cartIsShow, setCartIsShow] = useState(false)
+  const [cartIsShow, setCartIsShow] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShow(true)
-  }
+    setCartIsShow(true);
+  };
 
   const closeCartHandler = () => {
-    setCartIsShow(false)
-  }
+    setCartIsShow(false);
+  };
 
   return (
     <CartProvider>
-      {cartIsShow && < Cart onClose={closeCartHandler}/>}
+      {cartIsShow && <Cart onClose={closeCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
