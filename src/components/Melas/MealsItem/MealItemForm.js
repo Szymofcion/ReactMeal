22fbb7880fbed45424e33af1abfe4a6ed512612d
Szymofcion <Szymon.dziewajs@gmail.com>
2023-01-05 09:55:34 +0100

    added login Cart

--- src/components/Melas/MealsItem/MealItemForm.js
+++ src/components/Melas/MealsItem/MealItemForm.js
@@ -1,14 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from './MealItemForm.module.css'
 import Input from "../../UI/Input";
 
 const MealItem = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true)
     const amountInputRef = useRef();
 
     const submitHandler = e => {
         e.preventDefault();
 
         const enteredAmount = amountInputRef.current.value
+        const enteredAmountNumber = +enteredAmount
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false)
+            return
+        }
+
+        props.onAddToCart(enteredAmountNumber)
     }
 
     return (
@@ -22,6 +30,7 @@ const MealItem = (props) => {
                 defaultValue: '1',
             }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     )
 }
